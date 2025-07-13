dcc3956aaa4108b58797380ac5607a864a2f9c98
BlazingInferno <truedarkenergy@gmail.com>
2025-07-13 11:34:56 +0100

    Make Logo Clickable to Home Page

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -23,9 +23,9 @@ function Header() {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16">
           <div className="flex items-center">
-            <div className="flex-shrink-0">
-              <img src="/unnamed.png" alt="Blazing IT Logo" className="h-16 w-16" />
-            </div>
+            <Link to="/" className="flex-shrink-0">
+              <img src="/unnamed.png" alt="Blazing IT Logo" className="h-16 w-16 hover:opacity-80 transition-opacity" />
+            </Link>
             {/* Desktop Navigation */}
             <div className="hidden md:block">
               <div className="ml-10 flex items-baseline space-x-8">
